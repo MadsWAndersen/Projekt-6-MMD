c1a8ab482f847584f05d35773af3020e6074060b
SigneMH <54638106+SigneMH@users.noreply.github.com>
2019-12-05 20:23:41 +0100

    log-ind + opret-bruger update
    Added "opret-bruger" and updated the "log ind" screen. HTML and CSS should run smootly on both screens now.

---
+++ app/opret-bruger.js
@@ -0,0 +1,69 @@
+
+// Selecting all text elements //
+
+var email = document.forms["vform"]["email"];
+var phone = document.forms["vform"]["phone"];
+var pass = document.forms["vform"]["pass"];
+
+// Getting all error display objects //
+var email_error = document.getElementById('email_error')
+var phone_error = document.getElementById('phone_error')
+var pass_error = document.getElementById('pass_error')
+
+
+// Setting all event listeners //
+
+email.addEventListener("blur", emailVerify, true);
+phone.addEventListener("blur", phoneVerify, true);
+pass.addEventListener("blur", passVerify, true);
+
+
+
+// Validation function //
+
+function Validate(){
+// email validation //
+
+  if (email.value == "") {
+    email_error.textContent = "E-mail påkrævet";
+    email.focus();
+    return false;
+  }
+
+// Phone number validation//
+  if (phone.value == "") {
+    phone_error.textContent = "Telefon nummer påkrævet";
+    phone.focus();
+    return false;
+  }
+
+// password validation
+  if (pass.value == "") {
+    pass_error.textContent = "Adgangskode påkrævet";
+    pass.focus();
+    return false;
+  }
+}
+
+// Event handler functions
+function emailVerify() {
+  if (email.value != "") {
+    email_error.innerHTML = "";
+    return true;
+  }
+}
+
+function phoneVerify() {
+  if (phone.value != "") {
+    phone_error.innerHTML = "";
+    return true;
+  }
+}
+
+
+function passVerify() {
+  if (pass.value != "") {
+    pass_error.innerHTML = "";
+    return true;
+  }
+}
