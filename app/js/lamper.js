2b5944679f926dab67eea3d8bfe4b39d884b8db6
Mads W <madswandersen@hotmail.com>
2019-12-08 19:40:55 +0100

    Advarselslamper
    Tilføjet struktur i HTML + rykket JS filen

---
+++ app/js/lamper.js
@@ -0,0 +1,15 @@
+
+function changeImage(){
+    document.getElementById("showcase").src = ""
+}
+
+function changeImage() {
+
+       if (document.getElementById("showcase").src = "")
+       {
+           document.getElementById("showcase").src = "img/advarselslamper/masterwarning"
+       }
+       else
+       {
+           document.getElementById("showcase").src = "";
+       }
