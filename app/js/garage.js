65485c9d8fe70c3a8b22889b0dca68840556968e
TrineBuxbom <trinebuxbom@gmail.com>
2019-12-09 13:21:32 +0100

    Fikser slet js

--- app/js/garage.js
+++ app/js/garage.js
@@ -5,9 +5,3 @@
     let cardElement = document.getElementById(cardId);
   cardElement.parentNode.removeChild(cardElement);
  }
-
-
-//Slet bil
-function popupSlet() {
-  var popup = document.getElementById("popupKey");
-  popup.classList.toggle("show");
