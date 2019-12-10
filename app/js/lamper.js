6150ee0d49a67caa8b8b951171f2859f56307989
Mads W <madswandersen@hotmail.com>
2019-12-10 14:36:41 +0100

    Værksted-kort + værksted + lamper
    100% responsive nu

--- app/js/lamper.js
+++ app/js/lamper.js
@@ -1,30 +1,8 @@
-// let billeder = ['img/advarselslamper/gas.png', 'img/advarselslamper/heat.png', 'img/advarselslamper/seat.png']
-// let show = document.getElementById('showcase');
-//
-// // Event handler //
-//
-// function changeImage(){
-//   show.style.backgroundImage ="billeder[2]"
-// }
+// Select lamp function //
 
 function upDate(previewPic){
   let div = document.getElementById('placeholder');
   div.style.backgroundImage = "url('" + previewPic.src + "')";
   document.getElementById('alt-text').innerHTML = previewPic.alt;
 
-}
-
-
-var divItems = document.getElementsByClassName("knap1");
-
-function selected(item) {
-    this.clear();
-    item.style.backgroundColor = 'red';
-}
-
-function clear() {
-    for(var i=0; i < divItems.length; i++) {
-        var item = divItems[i];
-        item.style.backgroundColor = 'white';
-    }
 }
