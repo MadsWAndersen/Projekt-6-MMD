1c2e5e08f3ac39b9de033559e084235402193ae8
Rasmus Frost <ucl@rasmusfrost.dk>
2019-12-10 17:16:29 +0100

    Calendar done
    Calendar done. Tweaks can be made. No weekdays added.
    
    In progress: responsiveness on main

--- app/js/booktid.js
+++ app/js/booktid.js
@@ -1,4 +1,6 @@
 let dt = new Date();
+function renderDate(){
+let endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0 ).getDate();
 
 let months = ["Januar","Februar","Marts","April","Maj","Juni","Juli","August","September","Oktober","November","December"]
 
@@ -9,31 +11,38 @@ document.getElementById("month").innerHTML = months[dt.getMonth()];
 
 
 let cells = "";
-const date = new Date();
-const currentDay = date.getDate();
+const currentDay = dt.getDate();
 //Udkommenteret er en anden måde at skrive ovenstående på
 // const currentDay = (new Date()).getDay();
 
-for (i = 1; i<=30; i++){
-
-
+for (i = 1; i<=endDate; i++){
   // udkommenteret er en anden måde at skrive det nedenunder på
   // const dateClass = i === currentDay ? 'today' : '';
-
   let dateClass = '';
-
   if (i === currentDay) {
     dateClass = 'today';
   }
-
-	cells+= '<div class="nemlig ' + dateClass + '">' + i + "</div>";
+  //tilføjer en div som child til 'kalenderwrapper'-classen i HTML.
+	cells += '<div class="kald ' + dateClass + '">' +"<p>" + i + "</p>" + "</div>";
 }
 
 
-
+console.log(new Date);
 
 
 
 
 
 document.getElementsByClassName('kalenderwrapper')[0].innerHTML = cells;
+
+};
+
+
+function moveDate(para) {
+            if(para == "prev") {
+                dt.setMonth(dt.getMonth() - 1);
+            } else if(para == 'next') {
+                dt.setMonth(dt.getMonth() + 1);
+            }
+            renderDate();
+        }
