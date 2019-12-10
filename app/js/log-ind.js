96e2789ec6ce8cc04cfe06cf6b3cbf4a2293b8b5
SigneMH <54638106+SigneMH@users.noreply.github.com>
2019-12-10 13:27:44 +0100

    log-ind update
    js is finally working

--- app/js/log-ind.js
+++ app/js/log-ind.js
@@ -1,26 +1,89 @@
+//
+// // Selecting all text elements //
+//
+// var email = document.forms["vform"]["email"];
+// var pass = document.forms["vform"]["pass"];
+//
+// // Getting all error display objects //
+// var email_error = document.getElementById('email_error')
+// var pass_error = document.getElementById('pass_error')
+//
+//
+// // Setting all event listeners //
+//
+// email.addEventListener("blur", emailVerify, true);
+// pass.addEventListener("blur", passVerify, true);
+//
+//
+//
+//
+//
+// // Validation function //
+//
+// function Validate() {
+//
+// // email validation //
+//
+//   if (email.value == "") {
+//     email_error.textContent = "E-mail påkrævet";
+//     email.focus();
+//     return false;
+//   }
+//
+// // password validation //
+//
+//   if (pass.value == "") {
+//     pass_error.textContent = "Adgangskode påkrævet";
+//     pass.focus();
+//     return false;
+//   }
+// }
+//
+//
+//
+//
+// // Event handler functions //
+//
+// function emailVerify() {
+//   if (email.value != "") {
+//     email_error.innerHTML = "";
+//     return true;
+//   }
+// }
+//
+// function passVerify() {
+//   if (pass.value != "") {
+//     pass_error.innerHTML = "";
+//     return true;
+//   }
+// }
+
+
+
 
 // Selecting all text elements //
 
 var email = document.forms["vform"]["email"];
+
 var pass = document.forms["vform"]["pass"];
 
 // Getting all error display objects //
 var email_error = document.getElementById('email_error')
+
 var pass_error = document.getElementById('pass_error')
 
 
 // Setting all event listeners //
 
 email.addEventListener("blur", emailVerify, true);
+
 pass.addEventListener("blur", passVerify, true);
-
-
 
 
 
 // Validation function //
 
-function Validate() {
+function Validate(){
 
 // email validation //
   if (email.value == "") {
@@ -28,6 +91,7 @@ function Validate() {
     email.focus();
     return false;
   }
+
 
 // password validation
   if (pass.value == "") {
@@ -37,9 +101,6 @@ function Validate() {
   }
 }
 
-
-
-
 // Event handler functions
 function emailVerify() {
   if (email.value != "") {
@@ -47,6 +108,7 @@ function emailVerify() {
     return true;
   }
 }
+
 
 function passVerify() {
   if (pass.value != "") {
