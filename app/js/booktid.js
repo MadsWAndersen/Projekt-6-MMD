d378223627438c2e78e52f79ca9b7dcd5d93bdff
Rasmus Frost <ucl@rasmusfrost.dk>
2019-12-10 12:23:09 +0100

    Booktid edited

--- app/js/booktid.js
+++ app/js/booktid.js
@@ -16,5 +16,12 @@ let months = [
 ]
 
 
-document.getElementById("month").innerHTML = months[dt.getMonth()];
+
 document.getElementById("date_string").innerHTML = dt.toDateString();
+document.getElementById("month").innerHTML = months[dt.getMonth()];
+
+let cells = "";
+for (i = 1; i<=30; i++){
+	cells = "<div>" + i + "</div>";
+}
+document.getElementsByClassName('kalenderwrapper')[0].innerHTML = cells;
