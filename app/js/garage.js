8d53b21d6e1396219fefa61d4535f58ee6d9bb2c
TrineBuxbom <trinebuxbom@gmail.com>
2019-12-11 12:59:19 +0100

    Fix javascript

--- app/js/garage.js
+++ app/js/garage.js
@@ -1,10 +1,23 @@
+
+//Show hidden popup for bil 1
 function popupSlet() {
   var popup = document.getElementById("slet");
   popup.classList.toggle("show");
 }
 
-//Delete card
-  let removeCard = function(cardId) {
-    let cardElement = document.getElementById(cardId);
+//Show hiiden popup for bil 2
+function popupSlet2() {
+  var popup2 = document.getElementById("slet2");
+  popup2.classList.toggle("show");
+}
+//Slet kort 1
+  let fjernKort1 = function(fjern1) {
+    let cardElement = document.getElementById(fjern1);
   cardElement.parentNode.removeChild(cardElement);
  }
+
+ //Slet kort 2
+   let fjernKort2 = function(fjern2) {
+     let cardElement = document.getElementById(fjern2);
+   cardElement.parentNode.removeChild(cardElement);
+  }
