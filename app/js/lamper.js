523c2ef5c1f293b76f555fc06fb8c4000873f2c9
Rasmus Frost <ucl@rasmusfrost.dk>
2019-12-15 14:25:52 +0100

    Javascript merged into one file

--- app/js/lamper.js
+++
@@ -1,8 +0,0 @@
-// Select lamp function //
-
-function upDate(previewPic){
-  let div = document.getElementById('placeholder');
-  div.style.backgroundImage = "url('" + previewPic.src + "')";
-  document.getElementById('alt-text').innerHTML = previewPic.alt;
-
-}
